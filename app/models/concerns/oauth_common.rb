db91d83a45cc73cd895082a81b518a6dd0f793a5
Hao Truong <haotv03@gmail.com>
2026-02-23 10:23:56 +0700

    fix response

--- app/models/concerns/oauth_common.rb
+++ app/models/concerns/oauth_common.rb
@@ -10,7 +10,11 @@ module OauthCommon
     uid = data.dig(*provider_config[:uid_path])
     raise "UID not found in provider data" unless uid
 
-    user = find_or_initialize_by(uid: uid, provider: provider)
+    # Find by uid first, then fall back to email (handles Firebase-synced users)
+    user = find_by(uid: uid, provider: provider) ||
+           find_by(email: data["email"]) ||
+           new(provider: provider)
+    user.uid = uid
     user.email = data["email"]
     user.display_name = data["name"] || data["display_name"]
     user.photo_url = data["picture"] || data["photo_url"]
