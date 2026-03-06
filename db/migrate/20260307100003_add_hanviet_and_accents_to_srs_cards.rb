7a2462401d3e26230d9db505add299d0bc5013e6
Hao Truong <haotv03@gmail.com>
2026-03-07 01:22:19 +0700

    remove migration

--- db/migrate/20260307100003_add_hanviet_and_accents_to_srs_cards.rb
+++
@@ -1,6 +0,0 @@
-class AddHanvietAndAccentsToSrsCards < ActiveRecord::Migration[8.0]
-  def change
-    add_column :srs_cards, :hanviet, :string, limit: 200
-    add_column :srs_cards, :accents, :json
-  end
-end
