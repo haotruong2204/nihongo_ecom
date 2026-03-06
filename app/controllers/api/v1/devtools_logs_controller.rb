aa46b3060ebb6aa92da057fb6d135a5fbc8e5b51
Hao Truong <haotv03@gmail.com>
2026-03-06 14:26:54 +0700

    band IP

--- app/controllers/api/v1/devtools_logs_controller.rb
+++ app/controllers/api/v1/devtools_logs_controller.rb
@@ -36,10 +36,29 @@ class Api::V1::DevtoolsLogsController < ApplicationController
 
     log.save!
 
+    # Auto-ban IP if DevTools opened more than 15 times
+    if log.open_count > 15 && !BlockedIp.exists?(ip_address: ip)
+      BlockedIp.create!(
+        ip_address: ip,
+        reason: "Auto-banned: DevTools opened #{log.open_count} times"
+      )
+      ban_users_by_ip(ip)
+    end
+
     head :no_content
   end
 
   private
+
+  def ban_users_by_ip(ip_address)
+    DevtoolsLog.where(ip_address: ip_address).where.not(user_id: nil).includes(:user).find_each do |log|
+      user = log.user
+      next if user.nil? || user.banned?
+
+      user.update!(is_banned: true, banned_reason: "IP #{ip_address} blocked (DevTools auto-ban)")
+      user.update!(jti: SecureRandom.uuid)
+    end
+  end
 
   def try_authenticate
     token = request.headers["Authorization"].to_s.split.last
